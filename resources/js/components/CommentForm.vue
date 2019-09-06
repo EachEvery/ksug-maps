91effd60124fc15dcefed792fd6e9657dc27f6f8
Nate Hobi <nate.hobi@gmail.com>
2019-09-06 17:06:28 -0400

    wip

---
+++ resources/js/components/CommentForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <input
+      v-model="author"
+      type="text"
+      class="border-b border-black py-2 font-mono text-black"
+      placeholder="Your Name"
+    />
+    <input
+      v-model="email"
+      type="email"
+      class="border-b border-black py-2 font-mono text-black"
+      placeholder="Your Email Address"
+    />
+    <textarea
+      v-model="text"
+      class="border-b border-black p-2 font-mono text-black"
+      placeholder="Message Text"
+    ></textarea>
+
+    <clickable
+      class="w-full text-center py-3 px-2 border border-black uppercase"
+    >Submit Your Comment</clickable>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      author: "",
+      email: "",
+      text: ""
+    };
+  }
+};
+</script>
