7d0a54042b9a42c51f86941c76251a7408263a73
Nate Hobi <nate.hobi@gmail.com>
2020-07-14 11:54:44 -0400

    Oops -- fixes typo

--- resources/views/placeEmbedCode.blade.php
+++ resources/views/placeEmbedCode.blade.php
@@ -1,4 +1,4 @@
 <style type="text/css">.mm4{position:relative;color: transparent;display:inline-block;clear:both}.mm4::before{content:" ";position:absolute;left:0;right:0;top:0;bottom:0}.mm4 iframe{cursor:pointer!important}</style>
 <a class="mm4" href="{{$place->public_url}}">
-    <iframe width="600" heigh="450" src="{{$place->embed_url}}"></iframe>
+    <iframe width="600" height="450" src="{{$place->embed_url}}"></iframe>
 </a>
